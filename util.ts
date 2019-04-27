f4328bfec43aa828e811107ce5f3aed672a1d896
Vincent LE GOFF <g_n_s@hotmail.fr>
2019-04-27 19:26:34 +0200

    Add module `util` (#1)
    * bump to deno 0.3.10
    
    * util main
    
    * test addition
    
    * util
    
    * updated conf and CI
    
    * format
    
    * Types
    
    * types
    
    * format
    
    * fix CI mistake
    
    * missing funcs

---
+++ util.ts
@@ -0,0 +1,13 @@
+export { types } from "./util/types.ts";
+export {
+  isBoolean,
+  isNull,
+  isNullOrUndefined,
+  isNumber,
+  isString,
+  isSymbol,
+  isUndefined,
+  isObject,
+  isError,
+  isFunction
+} from "./util/main.ts";
