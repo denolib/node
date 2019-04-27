98398256cd3f828b0b4d9c46c8ac395284910b1f
LE GOFF Vincent <g_n_s@hotmail.fr>
2019-04-27 17:55:25 +0200

    format

--- util/main.ts
+++ util/main.ts
@@ -1,39 +1,39 @@
-export function isBoolean(value: unknown):boolean {
+export function isBoolean(value: unknown): boolean {
   return typeof value === "boolean" || value instanceof Boolean;
 }
 
-export function isNull(value: unknown):boolean {
+export function isNull(value: unknown): boolean {
   return value === null;
 }
 
-export function isNullOrUndefined(value: unknown):boolean {
+export function isNullOrUndefined(value: unknown): boolean {
   return value === null || value === undefined;
 }
 
-export function isNumber(value: unknown):boolean {
+export function isNumber(value: unknown): boolean {
   return typeof value === "number" || value instanceof Number;
 }
 
-export function isString(value: unknown):boolean {
+export function isString(value: unknown): boolean {
   return typeof value === "string" || value instanceof String;
 }
 
-export function isSymbol(value: unknown):boolean {
+export function isSymbol(value: unknown): boolean {
   return typeof value === "symbol";
 }
 
-export function isUndefined(value: unknown):boolean {
+export function isUndefined(value: unknown): boolean {
   return value === undefined;
 }
 
-export function isObject(value: unknown):boolean {
+export function isObject(value: unknown): boolean {
   return value !== null && typeof value === "object";
 }
 
-export function isError(e: unknown):boolean {
+export function isError(e: unknown): boolean {
   return e instanceof Error;
 }
 
-export function isFunction(value: unknown):boolean {
+export function isFunction(value: unknown): boolean {
   return typeof value === "function";
 }
